68e3e1e44a206e0e5ddd4c0a3843e6b4df136efe
Parmeshwar-Lahane <parmeshwar.lahane1991@gmail.com>
2024-08-08 11:26:51 +0530

    Add Initial files

---
+++ Addition.py
@@ -0,0 +1,8 @@
+def main():
+    no1 = int(input("Enter first number : "))
+    no2 = int(input("Enter second number : "))
+    Ans = no1 + no2
+    print("Addition of two number : ",Ans)
+
+if __name__=="__main__":
+    main()
